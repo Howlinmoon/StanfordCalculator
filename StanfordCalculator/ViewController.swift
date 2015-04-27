0a7abf38ba66bf5f4187c0592da170b717dce23a
Jim Veneskey <github@strifecrafter.com>
2015-04-26 20:11:02 -0400

    New supports the four basic math functions

--- StanfordCalculator/ViewController.swift
+++ StanfordCalculator/ViewController.swift
@@ -49,5 +49,38 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func operation(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+
+        // refactoring more and more from "X" onward
+        switch operation {
+            case "✖️": performOperation({ (op1: Double, op2: Double) -> Double in return op1 * op2})
+            
+            case "➗": performOperation({ (op1, op2)  in return op2 / op1})
+            
+            case "➕": performOperation({ (op1, op2)  in op1 + op2})
+            
+            case "➖": performOperation { $1 - $0}
+
+//            case "✔️": performOperation { sqrt($0)}
+
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        } else {
+            println("Not enough values on the stack, need minimum of 2")
+        }
+    }
+
+
+    
 }
 
