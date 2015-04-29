595b4d4f6d131f8344dc10127232506d99213170
Jim Veneskey <github@strifecrafter.com>
2015-04-29 13:09:25 -0400

    Implemented Calculator Brain, but even though followed demo exactly - getting compilation errors.

--- StanfordCalculator/StanfordCalulatorBrain.swift
+++ StanfordCalculator/StanfordCalulatorBrain.swift
@@ -34,9 +34,47 @@ class StanfordCalculatorBrain {
         knownOps["➖"] = Op.BinaryOperation("➖") { $1 - $0}
         knownOps["✔️"] = Op.UnaryOperation("✔️", sqrt)
     }
+    
+    
+    private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
+        
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            // Note - since all possibilities/cases of op are handled - no need for default case
+            switch op {
+            case .Operand(let operand):
+                return(operand, remainingOps)
+            
+            case .UnaryOperation(_, let operation):
+                let operandEvaluation = evaluate(remainingOps)
+                if let operand = operandEvaluation.result {
+                    return (operation(operand), operandEvaluation.remainingOps)
+                }
+                
+            case .BinaryOperation(_, let operation):
+                let op1Evaluation = evaluate(remainingOps)
+                if let operand1 = op1Evaluation.result {
+                    let op2Evaluation = evaluate(remainingOps)
+                    if let operand2 = op2Evaluation.result {
+                        return (operation(operand1, operand2), op2Evaluation.remainingOps)
+                    }
+                }
+            }
+        }
+        
+        return (nil, ops)
+    }
+    
+    func evaluate() -> Double? {
+        let (result, remainder) = evaluate(opStack)
+        return result
+    }
+    
  
-    func pushOperand(operand: Double) {
+    func pushOperand(operand: Double) -> Double? {
         opStack.append(Op.Operand(operand))
+        return evaluate()
     }
     
     func performOperation(symbol: String) {
